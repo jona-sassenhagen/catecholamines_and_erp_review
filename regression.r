4c51e057b4faf85e590331ca1d46a39eab073971
jona-sassenhagen <jona.sassenhagen@gmail.com>
2018-06-11 13:53:41 +0200

    add dosage

--- regression.r
+++ regression.r
@@ -1,6 +1,6 @@
 library(brms)
 
 df = read.table("meta_mix.csv", sep=",", header=1)
-fml = P3b ~ 1 + Dopamine_transporter + Noradrenaline_transporter + Alpha_1_Beta_adrenergic_receptor + D2_dopamine_receptor + Alpha_2_adrenergic_receptor + D1_dopamine_receptor + (1 | Study) + (1 | X)
+fml = P3b ~ 1 + Dopamine_transporter + Noradrenaline_transporter + Alpha_1_Beta_adrenergic_receptor + D2_dopamine_receptor + Alpha_2_adrenergic_receptor + D1_dopamine_receptor + (1 | Study) + (Dosage_.standardized. | X)
 mdl <- brm(fml, data=df, iter=5000, cores=2)
 summary(mdl)
